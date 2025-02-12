194527b567a1931c9da37df8eb1e88e6760c0644
Wilson Oliveira Lima <oliveiralima@protonmail.com>
2025-02-11 21:01:33 -0300

    Implement PWM control for LED with interrupt handling and smooth brightness adjustment

--- hello_pwm.c
+++ hello_pwm.c
@@ -1,34 +1,51 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-// Output PWM signals on pins 0 and 1
+#include <stdio.h>
 
 #include "pico/stdlib.h"
+#include "pico/time.h"
+
+#include "hardware/irq.h"
 #include "hardware/pwm.h"
 
-int main() {
-    /// \tag::setup_pwm[]
+#define PIN_LED 12
 
-    // Tell GPIO 0 and 1 they are allocated to the PWM
-    gpio_set_function(0, GPIO_FUNC_PWM);
-    gpio_set_function(1, GPIO_FUNC_PWM);
+int level = 0; // Nível de iluminação do LED
+bool rise = true; // Direção da variação de iluminação
 
-    // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
-    uint slice_num = pwm_gpio_to_slice_num(0);
+void wrap(){
+    pwm_clear_irq(pwm_gpio_to_slice_num(PIN_LED)); // Resetar o flag de interrupção para o slice
 
-    // Set period of 4 cycles (0 to 3 inclusive)
-    pwm_set_wrap(slice_num, 3);
-    // Set channel A output high for one cycle before dropping
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 1);
-    // Set initial B output high for three cycles before dropping
-    pwm_set_chan_level(slice_num, PWM_CHAN_B, 3);
-    // Set the PWM running
-    pwm_set_enabled(slice_num, true);
-    /// \end::setup_pwm[]
+    level = rise ? level + 1 : level - 1; // Incrementa ou decrementa o nível de iluminação
+    level = level > 255 ? 255 : level < 0 ? 0 : level; // Limita o nível de iluminação entre 0 e 255
+    rise = level == 255 ? false : level == 0 ? true : rise; // Inverte a direção da variação de iluminação
 
-    // Note we could also use pwm_set_gpio_level(gpio, x) which looks up the
-    // correct slice and channel for a given GPIO.
+    pwm_set_gpio_level(PIN_LED, level * level); // Define o nível de iluminação do LED (PWM) de forma quadrática (mais suave)
+}
+
+uint pwm_setup_irq(){
+    gpio_set_function(PIN_LED, GPIO_FUNC_PWM); // Configurar o pino GPIO para a função de PWM
+    
+    uint slice_enum = pwm_gpio_to_slice_num(PIN_LED); // Obter o slice do PWM associado ao pino GPIO (canal do PWM)
+
+    pwm_clear_irq(slice_enum); // Resetar o flag de interrupção para o slice
+    pwm_set_irq_enabled(slice_enum, true); // Habilitar a interrupção para o slice
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, wrap); // Definir um tipo de interrupção exclusiva para o wrap
+    irq_set_enabled(PWM_IRQ_WRAP, true); // Habilitar a interrupção de wrap
+
+    pwm_config config = pwm_get_default_config(); // Obter a configuração padrão do PWM
+    pwm_config_set_clkdiv(&config, 10.0f); // Define o divisor de clock do PWM
+    pwm_init(slice_enum, &config, true); // Inicializa o slice do PWM com a configuração definida
+
+    return slice_enum;
+}
+
+int main(){
+    uint slice_enum = pwm_setup_irq();
+
+    while(true) {
+        pwm_set_irq_enabled(slice_enum, true);
+        sleep_ms(5000);
+
+        pwm_set_irq_enabled(slice_enum, false);
+        sleep_ms(5000);
+    }
 }
