1663163899ac362a8497eb07edf750666a975f7f
Wilson Oliveira Lima <oliveiralima@protonmail.com>
2025-02-11 21:24:16 -0300

    Fix variable naming for PWM slice in LED setup function

--- hello_pwm.c
+++ hello_pwm.c
@@ -25,18 +25,18 @@ void wrap() {
 uint led_pwm_setup_irq() {
     gpio_set_function(PIN_LED, GPIO_FUNC_PWM); // Configurar o pino GPIO para a função de PWM
     
-    uint slice_enum = pwm_gpio_to_slice_num(PIN_LED); // Obter o slice do PWM associado ao pino GPIO (canal do PWM)
+    uint slice_num = pwm_gpio_to_slice_num(PIN_LED); // Obter o slice do PWM associado ao pino GPIO (canal do PWM)
 
-    pwm_clear_irq(slice_enum); // Resetar o flag de interrupção para o slice
-    pwm_set_irq_enabled(slice_enum, true); // Habilitar a interrupção para o slice
+    pwm_clear_irq(slice_num); // Resetar o flag de interrupção para o slice
+    pwm_set_irq_enabled(slice_num, true); // Habilitar a interrupção para o slice
     irq_set_exclusive_handler(PWM_IRQ_WRAP, wrap); // Definir um tipo de interrupção exclusiva para o wrap
     irq_set_enabled(PWM_IRQ_WRAP, true); // Habilitar a interrupção de wrap
 
     pwm_config config = pwm_get_default_config(); // Obter a configuração padrão do PWM
     pwm_config_set_clkdiv(&config, 2.0f); // Define o divisor de clock do PWM
-    pwm_init(slice_enum, &config, true); // Inicializa o slice do PWM com a configuração definida
+    pwm_init(slice_num, &config, true); // Inicializa o slice do PWM com a configuração definida
 
-    return slice_enum;
+    return slice_num;
 }
 
 uint servo_pwm_setup() {
